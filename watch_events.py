aaf54dc67b07d8c55cc8924f057baf5c0b0bac34
ikushi0703-creator <ikushi0703@gmail.com>
2026-06-05 19:22:49 +0900

    Update watch_events.py

--- watch_events.py
+++ watch_events.py
@@ -1,1 +1,21 @@
-print("Event watcher is running")
+import os
+import smtplib
+from email.message import EmailMessage
+
+gmail = os.environ["GMAIL_ADDRESS"]
+password = os.environ["GMAIL_APP_PASSWORD"]
+
+msg = EmailMessage()
+msg["Subject"] = "大学イベント監視システム テスト"
+msg["From"] = gmail
+msg["To"] = gmail
+
+msg.set_content(
+    "GitHub Actions からのテストメールです。"
+)
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+    smtp.login(gmail, password)
+    smtp.send_message(msg)
+
+print("mail sent")
